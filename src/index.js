093d92bfc9d2e91b60d2978b6a1d0faa581ecac3
jeben-hub <abondaronok@gmail.com>
2020-10-19 23:30:01 +0300

    feat: add check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	if (str.length % 2 != 0) return false;
+	let temp = str.length;
+	for (let i = 0; i < temp; i++) {
+		bracketsConfig.forEach(el => {
+			str = str.replace(el[0] + el[1], "");
+		});
+	}
+	return str.length == 0;
 }
